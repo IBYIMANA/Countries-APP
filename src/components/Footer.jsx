9901158b1d0ee0aab474c86053d93b971c76b3e0
IBYIMANA <filletteibyimana@gmail.com>
2024-04-02 10:12:16 +0200

    3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-  <div className='flex justify-between items-center bg-slate-50'>
-    <div className='pr-40 pl-40 mt-4'>
+  <div className='flex items-center justify-between bg-slate-50'>
+    <div className='pl-40 pr-40 mt-4'>
       <img src="./public/images/logo.png" alt="" />
       </div>
-      <p className='float-right pr-40 pl-40'>Copyright © 2024. All rights reserved.</p>
+      <p className='float-right pl-40 pr-40'>Copyright © 2024. All rights reserved.</p>
 
    
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,15 +8,15 @@ const NavBar = () => {
      <img src="./public/images/logo.png" alt="" />
       </li>
     </ul>
-    <ul className='flex float-right space-x-4 pr-40 mt-9'>
+    <ul className='flex float-right pr-40 space-x-4 mt-9'>
      <li>
-        <a href="#" className='hover:text-blue-700'>countries</a>
+        <a href="#" className='hover:text-blue-700'>Countries</a>
       </li>
       <li>
-        <a href="#" className='hover:text-blue-700'>continents</a>
+        <a href="#" className='hover:text-blue-700'>Continents</a>
       </li>
       <li>
-        <a href="#" className='hover:text-blue-700'>regions</a>
+        <a href="#" className='hover:text-blue-700'>Regions</a>
       </li>
     </ul>
    </nav>
